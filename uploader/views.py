38112e79e1b82f6f76f2bdea352616814a943aaf
Yiran Shu <yirans@sfu.ca>
2020-05-19 15:48:43 -0700

    upload source files

---
+++ uploader/views.py
@@ -0,0 +1,20 @@
+import os
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def upload(request):
+    return render(request, 'uploader/upload.html', {})
+
+
+def write_file(request):
+    if request.method == "POST":
+        myFile = request.FILES.get("myfile", None)
+        if not myFile:
+            return HttpResponse("no files for upload!")
+        destination = open(os.path.join("D:\\upload", myFile.name), 'wb+')
+        for chunk in myFile.chunks():
+            destination.write(chunk)
+        destination.close()
+        return HttpResponse(myFile.name + " upload over!")
+
