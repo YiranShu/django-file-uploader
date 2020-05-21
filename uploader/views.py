2f2b40511fbaf1af3c4d55faf9d28a2451307014
Yiran Shu <yirans@sfu.ca>
2020-05-20 18:36:16 -0700

    remove debug information

--- uploader/views.py
+++ uploader/views.py
@@ -1,7 +1,6 @@
 import os
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
 from . import models
 
 
@@ -15,22 +14,21 @@ def save_to_database(request):
         if not myFile:
             return HttpResponse("no files for upload!")
 
-        scene = models.Scene(
+        models.Scene(
             file=myFile, 
             file_name=myFile.name,
             scene_name=request.POST.get("scene_name"),
             description=request.POST.get("description"),
             category=request.POST.get("category"),
             tag=request.POST.get("tag"),
-            date_created=datetime.datetime.now()
         ).save()
-        print(type(request.POST.get("scene_name")))
+
         return HttpResponse(myFile.name + " upload over!")
 
 
 def retrieve_from_database(request, scene_id):
     scenes = models.Scene.objects(file_name=scene_id)
-    print(len(scenes))
+
     for scene in scenes:
         file = scene.file
 
@@ -42,7 +40,6 @@ def retrieve_from_database(request, scene_id):
             chunk = file.read(size=4000)
 
         destination.close()
-        print(scene.scene_name)
         output = scene.scene_name + " " + scene.description + " " + scene.category + " " +scene.tag + " " + str(scene.date_created)
         
     return HttpResponse(scene_id + " has been saved! " + output)
